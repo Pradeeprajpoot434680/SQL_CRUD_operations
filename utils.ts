f71d15a2d162b53804048fde40731c4791f75a3f
Pradeeprajpoot434680 <prrajpoot12234@gmail.com>
2025-05-02 17:27:09 +0530

    add sql crud operatoins

--- utils.ts
+++
@@ -1,9 +0,0 @@
-
-
-import { Client } from "pg";
-export async function getClient()
-{
-    const client = new Client('postgresql://my_sql_database_owner:lS6UCuaRPJ7V@ep-proud-smoke-a873yuj4-pooler.eastus2.azure.neon.tech/my_sql_database?sslmode=require');
-    await client.connect();
-    return client;
-}
